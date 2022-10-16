e2bb68ef9a80d9987617d4d9b49768a823183233
Marta Szymańska <aniakam666@gmail.com>
2022-10-16 17:41:29 +0200

    done

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -9,3 +9,22 @@
     Powodzenia!
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+
+function validateForm() {
+  let firstName = document.forms["myForm"]["firstname"].value;
+  let lastName = document.forms["myForm"]["lastname"].value;
+  let email = document.forms["myForm"]["email"].value;
+
+  if (firstName == "") {
+    alert("Wpisz swoje imię.");
+    return false;
+  }
+  if (lastName == "") {
+    alert("Wpisz swoje nazwisko.");
+    return false;
+  }
+  if (email == "") {
+    alert("Podaj swój adres email.");
+    return false;
+  }
+}
